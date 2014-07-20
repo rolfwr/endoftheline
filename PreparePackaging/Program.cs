df32863de86a257ce462bb84e721e21e3e30ba0e
Rolf W. Rasmussen <rolfwr@gmail.com>
2014-07-21 00:05:46 +0200

    Avoid unintended line breaking in license text shown by .vsix installer.
    The PreparePackaging project will create a RTF file based on the license.txt
    file which the .vsix installer will word-wrap to a suitable width.

---
+++ PreparePackaging/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace PreparePackaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var plainIn = new FileInfo(Path.Combine(dir.Parent.Parent.Parent.FullName, "EndOfTheLine", "license.txt"));
+            var rtfOut = new FileInfo(Path.GetFileNameWithoutExtension(plainIn.Name) + ".rtf");
+
+            if (args.Length > 0)
+            {
+                plainIn = new FileInfo(args[0]);
+            }
+
+            if (args.Length > 1)
+            {
+                rtfOut = new FileInfo(args[1]);
+            }
+
+            using (var rtfWriter = rtfOut.CreateText())
+            {
+                using (var plainReader = plainIn.OpenText())
+                {
+                    WriteToRtf(plainReader, rtfWriter);
+                }
+            }
+            Console.WriteLine("Wrote " + rtfOut.FullName);
+        }
+
+        private static void WriteToRtf(TextReader plainReader, TextWriter rtfWriter)
+        {
+            rtfWriter.WriteLine("{\\rtf");
+            rtfWriter.WriteLine("{\\fonttbl {\\f0 Times New Roman;}}");
+            rtfWriter.Write("\\f0\\fs22");
+
+            string prefix = " ";
+            while (true)
+            {
+                var line = plainReader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                Debug.Assert(line.IndexOfAny(new[] { '{', '\\' }) == -1,
+                    "Input that needs escaping not supported.");
+
+                if (line == string.Empty)
+                {
+                    rtfWriter.WriteLine("\\par");
+                    rtfWriter.WriteLine("\\par");
+                    prefix = string.Empty;
+                    continue;
+                }
+
+                rtfWriter.Write(prefix);
+                rtfWriter.Write(line);
+                prefix = " ";
+            }
+            rtfWriter.WriteLine();
+            rtfWriter.WriteLine("}");
+        }
+    }
+}
